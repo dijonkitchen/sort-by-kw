268b2483c1c7fdf4a54de8bd870e3d5e1fc593dd
JC <11434205+dijonkitchen@users.noreply.github.com>
2019-12-18 08:01:23 -0800

    feat: sort by keyword

--- src/sort_by_kw.clj
+++ src/sort_by_kw.clj
@@ -1,4 +1,23 @@
 (ns sort-by-kw)
 
+(defn keyword-comparator [kw-coll x y]
+  (let [reverse-coll (reverse kw-coll)]
+    (compare (.indexOf reverse-coll y)
+             (.indexOf reverse-coll x))))
+
+(comment
+  (compare 0 1)
+  (compare 0 -1)
+  (keyword-comparator [:low] :high :low)
+  (keyword-comparator [:low] :low :anything)
+  (keyword-comparator [:low] :high :anything))
+
+(defn sort-by-kw
+  "Sorts the `coll` ordered by keywords in `sort-vec`"
+  ([coll]
+   (sort-by-kw [] coll))
+  ([sort-vec coll]
+   (sort #(keyword-comparator sort-vec %1 %2) coll)))
+
 (defn -main []
   (println "Hola Multiverse!"))
--- test/sort_by_kw_test.clj
+++ test/sort_by_kw_test.clj
@@ -5,6 +5,44 @@
                          is]]
    [sort-by-kw :as sut]))
 
-(deftest sanity
-  (testing "equality"
-    (is (= 1 1))))
+(deftest keyword-comparator-test
+  (testing "exists"
+    (is sut/keyword-comparator))
+  (testing "takes two inputs to compare"
+    (is (sut/keyword-comparator [:high :low] :high :low)))
+  (testing "compares keywords in collection"
+    (is (= -1 (sut/keyword-comparator [:high :low] :high :low)))
+    (is (= 1 (sut/keyword-comparator [:high :low] :low :high)))
+    (is (= -1 (sut/keyword-comparator [:low :high] :low :high)))
+    (is (= -1 (sut/keyword-comparator [:low] :low :high)))
+    (is (= 1 (sut/keyword-comparator [:high] :low :high)))
+    (is (= 0 (sut/keyword-comparator [:high] :low :low)))
+    (is (= 0 (sut/keyword-comparator [] :low :low)))
+    (is (= 0 (sut/keyword-comparator [:med] :low :low)))
+    (is (= 0 (sut/keyword-comparator [:med] :low :high)))
+    (is (= 1 (sut/keyword-comparator [:high :med :low] :low :high)))))
+
+(deftest sort-by-kw-test
+  (testing "exists"
+    (is sut/sort-by-kw))
+  (testing "returns collection when empty"
+    (is (= []
+           (sut/sort-by-kw []))))
+  (testing "returns collection when only one item"
+    (is (= [:test]
+           (sut/sort-by-kw [:test]))))
+  (testing "sorts collection"
+    (is (= [:low :high]
+           (sut/sort-by-kw [:low :high])))
+    (is (= '(:low :high)
+           (sut/sort-by-kw [:low :high])))
+    (is (= '(:low :high)
+           (sut/sort-by-kw '(:low :high))))
+    (is (= '(:high :low)
+           (sut/sort-by-kw #{:low :high})))
+    (is (= '(:low :high :anything)
+           (sut/sort-by-kw [:low] [:high :low :anything])))
+    (is (= '(:low :high :anything)
+           (sut/sort-by-kw [:low :foo] [:high :low :anything])))
+    (is (= '(:low :foo :high :anything)
+           (sut/sort-by-kw [:low :foo] [:high :low :anything :foo])))))
